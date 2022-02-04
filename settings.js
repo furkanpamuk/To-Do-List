3d3f44c9d8c63fd71bc77bacce430930a866a936
Furkan Pamuk <furkanpamuk-@hotmail.com>
2022-02-05 00:33:30 +0300

    fixed deleteTask

--- settings.js
+++ settings.js
@@ -5,38 +5,41 @@ console.log(allLiDOM)
 
 let BtnDOM = document.querySelector('#liveToastBtn')
 
-BtnDOM.addEventListener('onclick',newElement)
+BtnDOM.addEventListener('onclick', newElement)
 
 inputDOM.addEventListener('keydown', (event) => {
-    if (event.key === "Enter" ) {
-        newElement() 
+    if (event.key === "Enter") {
+        newElement()
     }
-} )
+})
 
 let ulDOM = document.querySelector('#list')
 
-function newElement(event){
+
+
+function newElement(event) {
     let liDOM = document.createElement('li')
-    liDOM.innerHTML = `${inputDOM.value} <span>X</span>`
+    let newSpan = document.createElement('span')
+    newSpan.innerText = "X"
+    liDOM.innerHTML = inputDOM.value
     ulDOM.append(liDOM)
+    liDOM.append(newSpan)
     inputDOM.value = ""
-    liDOM.addEventListener('click',addClass)
+    liDOM.addEventListener('click', addClass)
+    newSpan.addEventListener('click', deleteTask)
 }
 
-allLiDOM.forEach ( item => item.addEventListener('click',addClass))
+allLiDOM.forEach(item => item.addEventListener('click', addClass))
 
-function addClass(event){
-    this.className == "checked" ?this.classList.remove("checked") :this.classList.add("checked")
-    
+function addClass(event) {
+    this.className == "checked" ? this.classList.remove("checked") : this.classList.add("checked")
+
 }
-
 console.log(allLiDOM)
 
 let spanDOM = document.querySelectorAll('ul#list>li>span')
-console.log(spanDOM)
+spanDOM.forEach(item => item.addEventListener('click', deleteTask))
+function deleteTask(event) {
 
-spanDOM.forEach( item => item.addEventListener('click',deleteTask))
-
-function deleteTask(event){
     this.parentElement.remove();
 }
